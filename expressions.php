3cd08fe93ebcdb43acd2c6ad74bea3598e74df92
Khairul25556 <krft44@gmail.com>
2025-05-29 09:00:32 +0600

    Initial commit

---
+++ expressions.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome to the PHP\n";
+
+echo $name = "Khairul";
+echo "\nWelcome ", $name,".Thank you\n";
+
+$pizza = 3;
+$pizzaSlice = 8;
+$total = $pizza * $pizzaSlice;
+echo "The total pizza slice are: ", $total, "\n";
+
+$isHungry = false;
+echo "Hungry? ";
+echo $isHungry? "Yes" : "no";
+echo "\n";
---
+++ types.php
@@ -0,0 +1,13 @@
+<?php
+$a = -7;
+var_dump($a, true, $a == true);
+echo "\n";
+
+
+$num1 = "15";
+$num2 = 10;
+echo $sum = $num1 + $num2;
+echo "\n";
+var_dump($num1);
+var_dump($num2);
+echo $num1===$num2 ? "Int" : "Diff";
