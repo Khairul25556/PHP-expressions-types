ee873b4b84cfe87f9cc92202d4f51d8e335a169b
Khairul25556 <krft44@gmail.com>
2025-06-17 23:33:03 +0600

    learned about static classes

---
+++ classes/static-classes.php
@@ -0,0 +1,106 @@
+<?php
+
+// class MathUtils{
+//     public static float $pi = 3.1416;
+//     public static function square(float $num): float{
+//         return $num * $num;
+//     }
+
+// }
+
+// var_dump(
+//     MathUtils::$pi,
+//     MathUtils::square(22)
+// );
+
+//Another Example
+//this not possible because its private. so we can do another way by using static
+
+// class Connection{
+//     private function __construct(){}
+// }
+
+// $connection = new Connection();
+
+//This is an example of the Singleton Design Pattern in PHP. It ensures that only one object of the class can ever exist at a time.
+class Connection{
+    private static $instance = null; //static → Belongs to the class, not to objects.
+    private function __construct(){}
+
+    public static function singleton(){
+        if(null === Connection::$instance){
+            Connection::$instance = new Connection();
+        }
+        return Connection::$instance;
+    }
+}
+
+$connection = Connection::singleton();
+
+// $connection = new Connection();
+
+//Another Example
+
+class Admin{
+    private static $instance = null; //$instance remembers the one and only object. 
+
+    private static $name = "Ghost";
+    private static $pass = 12324;
+
+    private function __construct(){} //No one can create an object from outside the class. I am  blocking new object creation directly. make sure only one object will be created
+
+    public static function getInstance(){ //if no admin exists yet, I will create one. If already exists, I will return the same one.
+        if(self::$instance === null){ //self" means this class's own $name (like $this -> name)
+            self::$instance = new Admin(); //object created
+        } 
+        return self::$instance;
+    }
+
+    public function logIn(){
+        if("Ghost" === Admin::$name && 12324 === Admin::$pass){
+            return "Admin is logged in";
+        } 
+
+        return "Log in failed! Try Again";
+    }
+}
+
+$admin = Admin::getInstance(); //I am getting the singleton object
+// $admin2 = Admin::getInstance();
+// if($admin1 === $admin2){
+//     echo "Both are same";
+// } else {
+//     echo "They are not same";
+// }
+
+echo $admin -> logIn(); // Calls the logIn function, prints result
+
+
+//Another example
+
+class Server{
+    private static $instance = null;
+
+    private static $ip = "192.178.60";
+
+    private function __construct(){}
+
+    public static function getInstance(){
+        if(self::$ip === null){
+            self::$ip = new Server();
+        }
+
+        return self::$ip;
+    }
+
+    public static function connection(){
+        if(self::$ip === "192.178.60"){
+            return "\nServer is connected!";
+        } 
+        return "\nServber lost connection!";
+    }
+}
+
+$connection = Server::connection();
+
+echo $connection;
