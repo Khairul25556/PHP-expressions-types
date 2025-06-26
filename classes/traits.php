35118e2392d318d4fbf1cbd1d3a08c69cdbfa54a
Khairul25556 <krft44@gmail.com>
2025-06-26 09:06:36 +0600

    learned about traits

---
+++ classes/traits.php
@@ -0,0 +1,38 @@
+<?php
+
+//traits is just a simple way to add methods to classes
+//traits don't necessarily have to be used with an interface
+
+//This says: “Any class that uses me must have a method called log()
+//it's like Hey, if you use this interface, I expect you to have a log() method.
+interface Logger{
+    public function log(string $message): void;
+}
+
+//A trait is a reusable block of code.
+//It gives you a ready-made method (log() here) that any class can use.
+//So you don’t have to write the same method in every class.
+
+trait Loggable{
+    public function log(string $message): void{
+        echo "Logging: $message\n";
+    }
+}
+
+//implements Logger: This means: “I will follow the Logger interface and provide a log() method.”
+//use Loggable;: This means: “Take the log() method from the trait and include it here.”
+class User implements Logger{
+    use Loggable;
+    public function __construct(public string $name){}
+
+    //log() is intended to output or record a message.
+    //You must define it — either inside the class, a parent class, or a trait.
+    //If you don’t define it, you’ll get a fatal error.
+
+    public function save(): void{
+        $this->log("User {$this->name} saved"); //for recorded custome message
+    }
+}
+
+$test = new User("Khairul");
+$test->save();
